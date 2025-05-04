10db3fc71ec9529e1a4c9c9b9653945aed0fc1d3
Jonatan Gezelius <boolean.adapter@gmail.com>
2025-05-05 00:53:06 +0200

    Rearrange Dockerfile for more efficient cache usage. Add more platforms to west allowlist.

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -8,7 +8,8 @@ ORIG_DIR=$(pwd)
 
 cd /opt/build
 #west build -p always -b esp32c6_devkitc/esp32c6 -- -DDTC_OVERLAY_FILE=boards/esp32c6.overlay
-$ORIG_DIR/buildcommand.sh
+echo invoking $ORIG_DIR/$1
+$ORIG_DIR/$1
 
 cd $ORIG_DIR
 mkdir -p bin
