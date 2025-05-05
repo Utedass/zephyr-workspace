bcf27de1b06220230b50429badf0ff23a8f34a45
Jonatan Gezelius <jonatan.gezelius@etteplan.com>
2025-05-05 13:36:13 +0200

    Fix newline issue in build.sh. Add better error handling in build.sh.

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 set -e
 
+echo "Cleaning up build directory.."
 rm -rf /opt/build/*
+echo "Copying work directory.."
 cp -r . /opt/build/
 
 ORIG_DIR=$(pwd)
 
 cd /opt/build
 #west build -p always -b esp32c6_devkitc/esp32c6 -- -DDTC_OVERLAY_FILE=boards/esp32c6.overlay
-echo invoking $ORIG_DIR/$1
+echo "Invoking build command from $ORIG_DIR/$1"
 $ORIG_DIR/$1
 
-cd $ORIG_DIR
-mkdir -p bin
-cp /opt/build/build/zephyr/zephyr.bin /opt/build/build/zephyr/zephyr.elf bin/
+echo "Copying artifacts back to work directory/bin.."
+artifact_endings=("bin" "elf" "uf2" "map")
+cd build/zephyr
+mkdir -p $ORIG_DIR/bin
+for ext in "${artifact_endings[@]}"; do
+    file="zephyr.${ext}"
+    if [[ -f $file ]]; then
+        cp $file $ORIG_DIR/bin/
+        echo "Copied $file"
+    else
+        echo "$file not present, skipping"
+    fi
+done
